f8eda4bc5d1113a5868e27e386d26981d8aef369
Valentin Bonnamour <valentinbonnamour@gmail.com>
2020-10-19 22:31:36 +0200

    V0.9 with CSS not optimized

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const navBar = document.querySelector("nav");
 const mainTitle = document.querySelector("h1");
+const goTop = document.querySelector("#goTop");
 const totalHeight = document.body.scrollHeight - window.innerHeight;
 
 
 window.onscroll = function () {
     let progressHeight = (window.pageYOffset / totalHeight) * 100;
     // progress.style.height = progressHeight + "%";
-    if (progressHeight > 4) {
+    if (progressHeight > 0) {
             navBar.classList.add("reducNavBar")
             mainTitle.style.fontSize = "25px"
             mainTitle.style.marginTop = "10px"
+            goTop.style.display = "flex";
             
             
 
@@ -20,6 +22,17 @@ window.onscroll = function () {
         navBar.classList.remove("reducNavBar")
         mainTitle.style.fontSize = "35px"
         mainTitle.style.marginTop = "20px"
+        goTop.style.display = "none"
     }
     console.log(progressHeight);
+    console.log(window.pageXOffset)
+    console.log(totalHeight);
   };
+
+  goTop.addEventListener('click', ()=>{
+      window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: "smooth"
+      })
+  })
